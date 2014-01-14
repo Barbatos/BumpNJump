76b6ace8774fac3f4462181ad0e5543bbd7e69ad
Charles Duprey <barbatos@urbanterror.info>
2014-01-14 09:33:51 +0100

    Renamed the functions class to resources

--- Rabbit.py
+++ Rabbit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 import pygame
-import Functions
+import Resources
 from pygame.locals import *
 
 class Rabbit(pygame.sprite.Sprite):
 	def __init__(self, id = -1, name = ""):
 		pygame.sprite.Sprite.__init__(self)
-		self.image, self.rect = Functions.loadPNG("test.png")
+		self.image, self.rect = Resources.loadPNG("test.png")
 		screen = pygame.display.get_surface()
 		self.area = screen.get_rect()
 		self.id = id
