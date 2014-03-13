832101f39823e388edfe77fab3d0d4ecc9810048
thornydre <thornydre@gmail.com>
2014-03-13 22:45:54 +0100

    Color finish again and again

--- Game.py
+++ Game.py
@@ -15,7 +15,7 @@ from PauseGameMenu import *
 from Map import *
 
 class Game():
-	def __init__(self, color1, color2):
+	def __init__(self):
 		self.butterflies = []
 		self.butterfliesSpritesList = pygame.sprite.Group()
 
